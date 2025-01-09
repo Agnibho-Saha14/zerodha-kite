9be68bb97243d9f229cea91e5a5804bbd9d6d3ed
Agnibho <sahaagnibho@gmail.com>
2025-01-09 20:27:43 +0530

    Minor changes

--- src/App.js
+++ src/App.js
@@ -65,26 +65,7 @@ const TradingPlatform = () => {
     </div>
   );
 
-  const Header = () => (
-    <div className="bg-white p-4 flex justify-between items-center mb-4">
-      <div className="flex items-center space-x-4">
-        <Menu className="w-6 h-6" />
-        <h1 className="text-xl font-bold">TradeUp</h1>
-      </div>
-      <div className="flex items-center space-x-4">
-        <div className="relative">
-          <Search className="w-5 h-5 absolute left-3 top-2.5 text-gray-400" />
-          <input 
-            type="text"
-            placeholder="Search stocks..."
-            className="pl-10 pr-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
-          />
-        </div>
-        <Bell className="w-6 h-6" />
-        <User className="w-6 h-6" />
-      </div>
-    </div>
-  );
+  
   const Navbar=()=>(
     
         <div className="navbar">
